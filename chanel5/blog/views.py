412f8e0294427c236ad146b38da48b9a07cd2302
aabsolute <aabsolute87@yahoo.co.jp>
2021-02-08 22:57:25 +0900

    20210208

--- chanel5/blog/views.py
+++ chanel5/blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from blog import models
+from django.views.generic import ListView, DetailView
+from django.views.generic.ArchiveIndexView import
 # Create your views here.
+
+class PostLV(ListView):
+    model = Bookmark
+    
+class PostDV(DetailView):
+    model = Bookmark
+
+class PostYAV():
+
+class PostMAV():
+
+class PostDAV():
+
+class PostTAV():
